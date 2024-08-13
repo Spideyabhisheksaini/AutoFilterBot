f5f57ed06d8c09a677bf7cd12e2ba70ac1952281
Spideyabhisheksaini <sainiabhi310@gmail.com>
2024-08-13 16:48:43 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -18,7 +18,7 @@ async def song(client, message):
     thumb_url = data['thumb']
     title = data['title']
     dura = data['duration']
-    songs = f"Title: {title}\nDuration: {dura}\nProvide by @Mrz_bots"
+    songs = f"Title: {title}\nDuration: {dura}\nProvided by @TonyStark_Botz"
     await m.edit("**📤 Uploading...**") 
     await message.reply_photo(photo=thumb_url, caption=songs)
     async with aiohttp.ClientSession() as session:
